cab34652024f73b8d9cc84203c6d5211b6772998
Jhonatanfc94 <jhonatanfc94@gmail.com>
2026-03-30 13:38:35 -0400

    mensaje calificar

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,22 @@
+// Archivo: src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom' // 👈 El semáforo mágico
-import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
 import './index.css'
+
+window.onerror = function(message) {
+  if (message.includes('initialization') || message.includes('loading chunk')) {
+    console.warn("Detectado error de caché. Limpiando Service Workers...");
+    navigator.serviceWorker.getRegistrations().then(registrations => {
+      for (let registration of registrations) {
+        registration.unregister();
+      }
+      // Forzar recarga limpia desde el servidor
+      window.location.reload(true);
+    });
+  }
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
