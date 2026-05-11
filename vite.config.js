9a3deaa4675ad09388ae55836070b331b3818a09
jhonatanfc94 <jhonatanfc94@gmail.com>
2026-05-11 15:37:40 -0400

    SEO google 2

--- vite.config.js
+++ vite.config.js
@@ -60,7 +60,15 @@ export default defineConfig({
             purpose: "any maskable"
           }
         ]
-      }
     })
   ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
